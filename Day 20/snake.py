831038d4572bc162cc1872e7e1a526ea40e9ac82
Alberto Rubinho Peruchi <116128604+AlbertoRP11@users.noreply.github.com>
2023-11-24 18:32:17 -0300

    Starting code of the snake game project

---
+++ Day 20/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [0, -20, -40]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+
+class Snake:
+
+    def __init__(self):
+        self.body = []
+        self.create_snake()
+        self.head = self.body[0]
+
+    def create_snake(self):
+        for x in STARTING_POSITIONS:
+            new_square = Turtle(shape="square")
+            new_square.color("white")
+            new_square.penup()
+            new_square.setx(x)
+            self.body.append(new_square)
+
+    def move(self):
+        # Move the blocks of the snake to the position of the blocks right in front of them
+        for index in range(len(self.body) - 1, 0, -1):  # start= len(self.body) - 1, stop= 0, step= -1
+            new_x = self.body[index - 1].xcor()
+            new_y = self.body[index - 1].ycor()
+            self.body[index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
