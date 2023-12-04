4aaa073b670e6b481dc0d6e09065f2d045dc0a89
Alberto Rubinho Peruchi <116128604+AlbertoRP11@users.noreply.github.com>
2023-12-04 10:51:47 -0300

    Improving the project using try/except

--- Day 26/NATO Alphabet/main.py
+++ Day 26/NATO Alphabet/main.py
@@ -8,8 +8,14 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Whats your word?").upper()
-# name_letters = [char for char in word]
-# nato_word = [code for (letter, code) in nato_dict.items() if letter in name_letters]
-nato_word = [nato_dict[letter] for letter in word]
-print(nato_word)
+def generate_phonetic():
+    word = input("Whats your word?").upper()
+    try:
+        nato_word = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please")
+        generate_phonetic()
+    else:
+        print(nato_word)
+
+generate_phonetic()
